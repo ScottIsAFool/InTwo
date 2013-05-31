bdc99d182cbe9e354a2bb9ffe2dcbe9ebbb4c3b7
Scott Lovegrove <scottisafool@live.co.uk>
2013-05-31 06:55:15 +0100

    Added retry logic to downloadsongsVM

--- InTwo/ViewModel/DownloadingSongsViewModel.cs
+++ InTwo/ViewModel/DownloadingSongsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Cimbalino.Phone.Toolkit.Services;
 using GalaSoft.MvvmLight.Command;
 using InTwo.Model;
@@ -34,49 +35,71 @@ namespace InTwo.ViewModel
             _settingsService = settingsService;
         }
 
-        public RelayCommand DownloadSongsPageLoaded
+        public bool RetryIsVisible { get; set; }
+
+        public RelayCommand DownloadDataCommand
         {
             get
             {
                 return new RelayCommand(async () =>
                 {
-                    if (!_navigationService.IsNetworkAvailable) return;
-
-                    try
-                    {
-                        var genresResponse = await _musicClient.GetGenresAsync();
-
-                        if (genresResponse.Error != null)
-                        {
-                            // TODO: Display an error
-                        }
-
-                        var genres = genresResponse.Result;
-
-                        _settingsService.Set("Genres", genres);
-                        _settingsService.Save();
-
-                        var tracks = new List<Product>();
-                        foreach (var genre in genres.Where(x => !x.Name.Equals("Comedy")))
-                        {
-                            var trackResponse = await _musicClient.GetTopProductsForGenreAsync(genre, Category.Track, 0, 100);
-
-                            if (trackResponse.Error == null)
-                            {
-                                tracks.AddRange(trackResponse.Result);
-                            }
-                        }
-
-                        await _asyncStorageService.WriteAllTextAsync(Constants.GameDataFile, await JsonConvert.SerializeObjectAsync(tracks));
-
-                        _navigationService.NavigateTo(Constants.Pages.MainPage + Constants.ClearBackStack);
-                    }
-                    catch (Exception ex)
-                    {
-                        // TODO: Display an error
-                    }
+                    await DownloadData();
                 });
             }
+        }
+
+        private async Task DownloadData()
+        {
+            if (!_navigationService.IsNetworkAvailable)
+            {
+                RetryIsVisible = true;
+
+                return;
+            }
+
+            RetryIsVisible = false;
+
+            try
+            {
+                ProgressIsVisible = true;
+                
+                var genresResponse = await _musicClient.GetGenresAsync();
+
+                if (genresResponse.Error != null || (genresResponse.Result == null))
+                {
+                    // TODO: Display an error
+                    RetryIsVisible = true;
+                    ProgressIsVisible = false;
+
+                    return;
+                }
+
+                var genres = genresResponse.Result;
+
+                _settingsService.Set("Genres", genres);
+                _settingsService.Save();
+
+                var tracks = new List<Product>();
+                foreach (var genre in genres.Where(x => !x.Name.Equals("Comedy")))
+                {
+                    var trackResponse = await _musicClient.GetTopProductsForGenreAsync(genre, Category.Track, 0, 100);
+
+                    if (trackResponse.Error == null)
+                    {
+                        tracks.AddRange(trackResponse.Result);
+                    }
+                }
+
+                await _asyncStorageService.WriteAllTextAsync(Constants.GameDataFile, await JsonConvert.SerializeObjectAsync(tracks));
+
+                _navigationService.NavigateTo(Constants.Pages.MainPage + Constants.ClearBackStack);
+            }
+            catch (Exception ex)
+            {
+                // TODO: Display an error
+            }
+
+            ProgressIsVisible = false;
         }
     }
 }
