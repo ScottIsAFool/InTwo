aaeb763c619120827d0707e7305bcc64c3e13f8a
Scott Lovegrove <scottisafool@live.co.uk>
2013-06-03 06:56:58 +0100

    Checks to see if the guess is correct (within 95%)

--- InTwo/Utils.cs
+++ InTwo/Utils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using GalaSoft.MvvmLight.Ioc;
 using Nokia.Music;
 using Nokia.Music.Types;
@@ -7,19 +8,79 @@ namespace InTwo
 {
     public static class Utils
     {
-        private static readonly Random _random = new Random();
+        private static readonly Random Random = new Random();
 
         internal static int GetRandomNumber(int minValue, int maxValue)
         {
-            var number = (_random.NextDouble() * maxValue) + minValue;
+            var number = (Random.NextDouble() * maxValue) + minValue;
 
             return (int)Math.Floor(number);
         }
 
-        public static Uri GetSampleUri(this Product product)
+        internal static Uri GetSampleUri(this Product product)
         {
             var client = SimpleIoc.Default.GetInstance<MusicClient>();
             return client.GetTrackSampleUri(product.Id);
+        }
+
+        private static readonly string[] IgnoredWords = {"the", "and", "a", "an"};
+
+        internal static void SeeIfWeHaveAWinner(Product gameTrack, string artistGuess, string songGuess, out bool artistGuessCorrect, out bool songGuessCorrect)
+        {
+            artistGuessCorrect = songGuessCorrect = false;
+
+            if (!string.IsNullOrEmpty(artistGuess))
+            {
+                foreach (var artist in gameTrack.Performers)
+                {
+                    if (CheckWords(artist, artistGuess, ref artistGuessCorrect))
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(songGuess))
+            {
+                CheckWords(gameTrack, songGuess, ref artistGuessCorrect);
+            }
+        }
+
+        private static bool CheckWords(MusicItem gameInfo, string guess, ref bool guessCorrect)
+        {
+            var splitWords = gameInfo.Name.ToLower().Split(new[] { ' ' }).ToList();
+            splitWords.ForEach(word =>
+            {
+                if (IgnoredWords.Contains(word))
+                {
+                    splitWords.Remove(word);
+                }
+            });
+
+            var guessSplitWords = guess.ToLower().Split(new[] { ' ' }).ToList();
+            guessSplitWords.ForEach(word =>
+            {
+                if (IgnoredWords.Contains(word))
+                {
+                    guessSplitWords.Remove(word);
+                }
+            });
+
+            var numberOfCorrectWords = 0;
+            guessSplitWords.ForEach(word =>
+            {
+                if (splitWords.Contains(word))
+                {
+                    numberOfCorrectWords++;
+                }
+            });
+
+            if (numberOfCorrectWords > (splitWords.Count * 0.9))
+            {
+                guessCorrect = true;
+                return true;
+            }
+            return false;
         }
     }
 }
