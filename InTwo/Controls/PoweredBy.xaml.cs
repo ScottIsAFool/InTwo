fcc350cad09d743e570b8a6c9056dc8258cf1b55
ScottIsAFool <scottisafool@live.co.uk>
2013-06-10 22:48:13 +0100

    Added PoweredBy to the about page

---
+++ InTwo/Controls/PoweredBy.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Tasks;
+
+namespace InTwo.Controls
+{
+    public partial class PoweredBy
+    {
+        public PoweredBy()
+        {
+            InitializeComponent();
+        }
+
+        private void NokiaMusicLink_OnClick(object sender, RoutedEventArgs e)
+        {
+            Windows.System.Launcher.LaunchUriAsync(new Uri("nokia-music://"));
+        }
+
+        private void ScoreoidLink_OnClick(object sender, RoutedEventArgs e)
+        {
+            new WebBrowserTask
+            {
+                Uri = new Uri("http://scoreoid.net", UriKind.Absolute)
+            }.Show();
+        }
+    }
+}
