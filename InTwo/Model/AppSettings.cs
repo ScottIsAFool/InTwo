112f4cbbc3f6fd29a10b10e992159b7eb272d169
Scott Lovegrove <scottisafool@live.co.uk>
2013-06-12 19:11:58 +0100

    Removed need for location service

--- InTwo/Model/AppSettings.cs
+++ InTwo/Model/AppSettings.cs
@@ -58,6 +58,18 @@ namespace InTwo.Model
                 MessageBox.Show("Really? You think you can do it in 0 seconds? Well, as good as you think you are, you have to select at least 1 second. Sorry.", "Sorry", MessageBoxButton.OK);
                 DefaultGameLength = TimeSpan.FromSeconds(2);
             }
+
+            AlertTheGameViewModel();
+        }
+
+        private void OnDefaultGenreChanged()
+        {
+            AlertTheGameViewModel();
+        }
+
+        private void AlertTheGameViewModel()
+        {
+            Messenger.Default.Send(new NotificationMessage(Constants.Messages.UpdateTheDefaultsManMsg));
         }
 
         private void OnUseProfilePictureInTileChanged()
