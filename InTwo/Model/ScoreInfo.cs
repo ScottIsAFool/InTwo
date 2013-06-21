cb8ef2b959339d359c4fb9ea9b7e8f12c29725de
Scott Lovegrove <scottisafool@live.co.uk>
2013-06-21 07:47:49 +0100

    Added ScoreInfo class

---
+++ InTwo/Model/ScoreInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace InTwo.Model
+{
+    public class ScoreInfo
+    {
+        public List<string> Genres { get; set; }
+        public int GameLength { get; set; }
+        public int NumberOfRounds { get; set; }
+    }
+}
