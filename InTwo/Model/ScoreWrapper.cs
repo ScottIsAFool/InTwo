02c320945d108dceeee4ab6ca48adf1fbc0c7b22
Scott Lovegrove <scottisafool@live.co.uk>
2013-06-21 07:13:08 +0100

    Added ScoreWrapper

---
+++ InTwo/Model/ScoreWrapper.cs
@@ -0,0 +1,12 @@
+﻿using PropertyChanged;
+using Scoreoid;
+
+namespace InTwo.Model
+{
+    [ImplementPropertyChanged]
+    public class ScoreWrapper
+    {
+        public score Score { get; set; }
+        public ScoreInfo ScoreInfo { get; set; }
+    }
+}
