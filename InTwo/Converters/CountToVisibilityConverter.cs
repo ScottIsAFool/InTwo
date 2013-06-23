70772c82f9bebcc728110b6409097bb1cf4a0ba3
ScottIsAFool <scottisafool@live.co.uk>
2013-06-23 17:44:35 +0100

    Scoreboard header hidden if score was 0

---
+++ InTwo/Converters/CountToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace InTwo.Converters
+{
+    public class CountToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return Visibility.Collapsed;
+            }
+
+            var count = int.Parse(value.ToString());
+
+            return count > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
